3cc4cdab486a8504fc6a0ee70a0eecb0aa42a76b
Paul Sosulin <pavelsosulin@gmail.com>
2022-06-16 13:28:47 +0400

    engineering holidays day 2

--- Snake_Game/snake_game.py
+++ Snake_Game/snake_game.py
@@ -41,3 +41,48 @@ def close_game():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+
+
+# игровой цикл
+while True:
+    surface.blit(img, (0, 0))
+    # отрисовка змейки
+    [pygame.draw.rect(surface, pygame.Color('green'), (i, j, SIZE - 1, SIZE - 1)) for i, j in snake]
+    # отрисовка яблок
+    pygame.draw.rect(surface, pygame.Color('red'), (*apple, SIZE, SIZE))
+
+    # отрисовка очков
+    render_score = font_score.render(f'ОЧКИ: {score}', 1, pygame.Color('blue'))
+    surface.blit(render_score, (5, 5))
+
+    # движение змейки
+    speed_count += 1 # speed_count = speed_count + 1
+    if not speed_count % snake_speed:
+        x += dx * SIZE
+        y += dy * SIZE
+        snake.append((x, y))
+        snake = snake[-length:]
+
+    # поедание еды
+    if snake[-1] == apple:
+        apple = randrange(SIZE, RES - SIZE, SIZE), randrange(SIZE, RES - SIZE, SIZE)
+        length += 1
+        score += 1
+        snake_speed -= 1 # snake_speed = snake_speed - 1
+        snake_speed = max(snake_speed, 4)
+
+
+
+    # конец игры
+    # x < 0 or x > RES - SIZE or y < 0 or y > RES - SIZE - выход за пределы игрового поля
+    # len(snake) != len(set(snake)) - голова змейки врезается в собственный хвост
+    if x < 0 or x > RES - SIZE or y < 0 or y > RES - SIZE or len(snake) != len(set(snake)):
+        while True:
+            render_end = font_end.render('КОНЕЦ ИГРЫ', 1, pygame.Color('blue'))
+            surface.blit(render_end, (RES // 2 - 200, RES // 3))
+            pygame.display.flip()
+            close_game()
+
+    pygame.display.flip()
+    clock.tick(fps)
+    close_game()
